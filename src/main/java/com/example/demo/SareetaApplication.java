8776076e6f61300f878a37626ff8013273a06342
ayoya22 <djoukamsimplice@gmail.com>
2019-09-02 12:50:46 +0100

    Completed jwt security
    Signed-off-by: ayoya22 <djoukamsimplice@gmail.com>

--- src/main/java/com/example/demo/SareetaApplication.java
+++ src/main/java/com/example/demo/SareetaApplication.java
@@ -1,13 +1,18 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableJpaRepositories("com.example.demo.model.persistence.repositories")
 @EntityScan("com.example.demo.model.persistence")
-@SpringBootApplication
+@EnableAutoConfiguration
+@Configuration
+@ComponentScan
 public class SareetaApplication {
 
 	public static void main(String[] args) {
